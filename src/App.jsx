e6a60bf344587fdaa40afa8d881bb80aa4119984
samridhi8595-lab <samridhi8595@gmail.com>
2025-12-15 15:24:28 +0530

    add key

--- src/App.jsx
+++ src/App.jsx
@@ -108,8 +108,10 @@ const App = () => {
   return (
     
     <div className='parent'>
-      {jobOpenings.map(function(elem){
-        return <Card company={elem.companyName} datePosted={elem.datePosted} post={elem.post} tag1={elem.tag1} tag2={elem.tag2} brandLogo={elem.brandLogo} pay={elem.pay}/>
+      {jobOpenings.map(function(elem,idx){
+        return <div key={idx}>
+         <Card company={elem.companyName} datePosted={elem.datePosted} post={elem.post} tag1={elem.tag1} tag2={elem.tag2} brandLogo={elem.brandLogo} pay={elem.pay}/>
+         </div>
       })}
       
     </div>
